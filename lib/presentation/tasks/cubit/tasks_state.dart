8adf84625f4357c69866be2a9474e36edf543155
King-Korol <korolnik2001@gmail.com>
2022-10-14 17:44:02 +0300

    new functionality

--- lib/presentation/tasks/cubit/tasks_state.dart
+++ lib/presentation/tasks/cubit/tasks_state.dart
@@ -1,13 +1,19 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:to_do_app/core/data/models/task_data_model.dart';
-import 'package:to_do_app/domain/tasks/models/single_task_domain_model.dart';
+import 'package:to_do_app/presentation/tasks/models/task_state_model.dart';
 
 part 'tasks_state.freezed.dart';
 
 @freezed
 class TasksState with _$TasksState {
-  const factory TasksState({
-    @Default(false) bool isLoading,
-    List<SingleTaskDomainModel>? tasks,
-  }) = _TasksState;
+  const factory TasksState.initial() = TasksInitialState;
+
+  const factory TasksState.loading() = TasksLoadingState;
+
+  const factory TasksState.networkError() = TasksNetworkErrorState;
+
+  const factory TasksState.unknownError() = TasksUnknownErrorState;
+
+  const factory TasksState.success({
+    required List<TaskStateModel> tasks,
+  }) = TasksSuccessState;
 }
